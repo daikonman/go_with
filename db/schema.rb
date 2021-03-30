4d511203705c89a1db9ad8845066de241f60fd78
daikonman <daiki457110@gmail.com>
2021-03-30 23:49:20 +0900

    Fix users_profile_test

--- db/schema.rb
+++ db/schema.rb
@@ -47,15 +47,6 @@ ActiveRecord::Schema.define(version: 2021_03_24_161748) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "likes", force: :cascade do |t|
-    t.integer "post_id", null: false
-    t.integer "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["post_id"], name: "index_likes_on_post_id"
-    t.index ["user_id"], name: "index_likes_on_user_id"
-  end
-
   create_table "microposts", force: :cascade do |t|
     t.string "content"
     t.integer "user_id", null: false
@@ -84,7 +75,5 @@ ActiveRecord::Schema.define(version: 2021_03_24_161748) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "likes", "posts"
-  add_foreign_key "likes", "users"
   add_foreign_key "microposts", "users"
 end
